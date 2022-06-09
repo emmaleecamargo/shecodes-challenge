7165fa4d5ad7fabf844bf7cf715db7790e763cc0
emmaleecamargo <emmaleecamargo@gmail.com>
2022-06-09 03:03:23 -0700

    Fixed API functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 function getDate(date) {
   let h2 = document.querySelector("h2");
-  let dayIndex = now.getDate();
-  let hours = now.getHours();
+  let dayIndex = date.getDate();
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = now.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day = days[now.getDay()];
+  let day = days[date.getDay()];
 
   let months = [
     "Jan",
@@ -27,16 +27,13 @@ function getDate(date) {
     "Nov",
     "Dec",
   ];
-  let month = months[now.getMonth()];
+  let month = months[date.getMonth()];
 
   h2.innerHTML = `${day}, ${month} ${dayIndex}, ${hours}:${minutes}`;
 }
 
 let now = new Date();
 getDate(now);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
 
 function displayWeather(response) {
   console.log(response.data);
@@ -53,7 +50,7 @@ function displayWeather(response) {
 
   let temperature = Math.round(celsiusTemperature);
   let humidity = response.data.main.humidity;
-  let windSpeed = response.data.main.wind.speed;
+  let windSpeed = response.data.wind.speed;
 
   cityTemp.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
@@ -69,26 +66,22 @@ function displayWeather(response) {
   }
   cityDescription.innerHTML = `Humidity: ${humidity}% Wind: ${windSpeed} km/h`;
 
-  getForecast(response.data.coord);
+  //getForecast(response.data.coord);
 }
-
-let cityInput = document.querySelector("#city-input");
-let apiKey = "96a225160ee7bd59b386370f8dd55115";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
 
 function search(city) {
   let apiKey = "96a225160ee7bd59b386370f8dd55115";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  console.log(cityInput.value);
+  search(cityInput.value);
 }
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
 
 search("Paris");
